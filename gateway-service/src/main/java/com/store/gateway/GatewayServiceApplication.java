d3792f6c114f0b9b48e49ebad7eba9cc82259fea
Omar Hernandez <ing_omarhdez@hotmail.com>
2021-02-15 11:51:24 -0600

    -

--- gateway-service/src/main/java/com/store/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/store/gateway/GatewayServiceApplication.java
@@ -3,11 +3,35 @@ package com.store.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
+	
+	@Bean
+	SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+		return http.httpBasic().and()
+				.csrf().disable()
+				.authorizeExchange()
+				.pathMatchers("/anything/**").authenticated()
+				.anyExchange().permitAll()
+				.and()
+				.build();
+	}
+
+	@Bean
+	public MapReactiveUserDetailsService reactiveUserDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+		return new MapReactiveUserDetailsService(user);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
